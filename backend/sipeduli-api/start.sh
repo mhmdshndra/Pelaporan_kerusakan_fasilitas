fe208d1f25fc89fcd6902e4241b4984ca533e6d3
mhmdshndra <mhmdshndra@gmail.com>
2026-05-25 20:36:33 +0700

    fix start.sh line endings

--- backend/sipeduli-api/start.sh
+++ backend/sipeduli-api/start.sh
@@ -2,4 +2,8 @@
 
 php artisan optimize:clear
 
-php artisan serve --host=0.0.0.0 --port=$PORT
+PORT_TO_USE=${PORT:-8080}
+
+echo "Running on port $PORT_TO_USE"
+
+php artisan serve --host=0.0.0.0 --port=$PORT_TO_USE
